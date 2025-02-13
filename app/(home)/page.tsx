c4cfda40c39b055c26959382af221f59d7907c48
rickyzatnika <rickyzatnika91@gmail.com>
2025-02-13 13:39:40 +0700

    added carousel

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,8 @@
-import { Button } from "@/components/ui/button";
+import { HomeCarousel } from "@/components/shared/home/home-carousel";
+import data from "@/lib/data";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Button</Button>
-    </div>
+    <HomeCarousel items={data.carousels} />
   );
 }
