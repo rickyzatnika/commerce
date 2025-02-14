76351d11ded6987e9fc072adbb650fe6a134c952
rickyzatnika <rickyzatnika91@gmail.com>
2025-02-14 18:19:45 +0700

    added cheackout page

--- hooks/use-cart-store.ts
+++ hooks/use-cart-store.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-import { Cart, OrderItem } from '@/types'
+import { Cart, OrderItem, ShippingAddress } from '@/types'
 import { calcDeliveryDateAndPrice } from '@/lib/actions/order.actions'
 
 const initialState: Cart = {
@@ -11,15 +12,19 @@ const initialState: Cart = {
   shippingPrice: undefined,
   totalPrice: 0,
   paymentMethod: undefined,
+  shippingAddress: undefined,
   deliveryDateIndex: undefined,
 }
 
 interface CartState {
   cart: Cart
   addItem: (item: OrderItem, quantity: number) => Promise<string>
-
   updateItem: (item: OrderItem, quantity: number) => Promise<void>
   removeItem: (item: OrderItem) => void
+  clearCart: () => void
+  setShippingAddress: (shippingAddress: ShippingAddress) => Promise<void>
+  setPaymentMethod: (paymentMethod: string) => void
+  setDeliveryDateIndex: (index: number) => Promise<void>
 }
 
 const useCartStore = create(
@@ -28,7 +33,7 @@ const useCartStore = create(
       cart: initialState,
 
       addItem: async (item: OrderItem, quantity: number) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const existItem = items.find(
           (x) =>
             x.product === item.product &&
@@ -62,19 +67,23 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
           },
         })
-        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-        return updatedCartItems.find(
+        const foundItem = updatedCartItems.find(
           (x) =>
             x.product === item.product &&
             x.color === item.color &&
             x.size === item.size
-        )?.clientId!
+        )
+        if (!foundItem) {
+          throw new Error('Item not found in cart')
+        }
+        return foundItem.clientId
       },
       updateItem: async (item: OrderItem, quantity: number) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const exist = items.find(
           (x) =>
             x.product === item.product &&
@@ -95,12 +104,13 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
           },
         })
       },
       removeItem: async (item: OrderItem) => {
-        const { items } = get().cart
+        const { items, shippingAddress } = get().cart
         const updatedCartItems = items.filter(
           (x) =>
             x.product !== item.product ||
@@ -113,32 +123,60 @@ const useCartStore = create(
             items: updatedCartItems,
             ...(await calcDeliveryDateAndPrice({
               items: updatedCartItems,
+              shippingAddress,
             })),
           },
         })
       },
+      setShippingAddress: async (shippingAddress: ShippingAddress) => {
+        const { items } = get().cart
+        set({
+          cart: {
+            ...get().cart,
+            shippingAddress,
+            ...(await calcDeliveryDateAndPrice({
+              items,
+              shippingAddress,
+            })),
+          },
+        })
+      },
+      setPaymentMethod: (paymentMethod: string) => {
+        set({
+          cart: {
+            ...get().cart,
+            paymentMethod,
+          },
+        })
+      },
+      setDeliveryDateIndex: async (index: number) => {
+        const { items, shippingAddress } = get().cart
 
+        set({
+          cart: {
+            ...get().cart,
+            ...(await calcDeliveryDateAndPrice({
+              items,
+              shippingAddress,
+              deliveryDateIndex: index,
+            })),
+          },
+        })
+      },
+      clearCart: () => {
+        set({
+          cart: {
+            ...get().cart,
+            items: [],
+          },
+        })
+      },
       init: () => set({ cart: initialState }),
     }),
+
     {
       name: 'cart-store',
     }
   )
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 export default useCartStore
--- types/index.ts
+++ types/index.ts
@@ -3,9 +3,10 @@ import {
   CartSchema,
   OrderItemSchema,
   ProductInputSchema,
+  ShippingAddressSchema,
   UserInputSchema,
-  UserSignUpSchema,
   UserSignInSchema,
+  UserSignUpSchema,
 } from '@/lib/validator'
 
 
@@ -31,12 +32,14 @@ export type Data = {
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 
 // user
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
 
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>
+
 
 
 
