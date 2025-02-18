c1ea1dc4c7fa328ad305403943acc75d7d5fba53
rickyzatnika <rickyzatnika91@gmail.com>
2025-02-19 00:59:02 +0700

    product not found

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -262,7 +262,7 @@ export default async function SearchPage(props: {
           </div>
 
           <div className='grid grid-cols-1 gap-4 md:grid-cols-2  lg:grid-cols-3  '>
-            {data.products.length === 0 && <div>No product found</div>}
+            {data.products.length === 0 && <div>Pencarian tidak ditemukan</div>}
             {data.products.map((product: IProduct) => (
               <ProductCard key={product._id} product={product} />
             ))}
