d083d5989d0e91d0862af9d7bee9627adb2abedb
rickyzatnika <rickyzatnika91@gmail.com>
2025-02-19 05:52:49 +0700

    fix responsive product slider

--- components/shared/product/product-slider.tsx
+++ components/shared/product/product-slider.tsx
@@ -36,13 +36,13 @@ export default function ProductSlider({
               key={product.slug}
               className={
                 hideDetails
-                  ? 'md:basis-1/4 lg:basis-1/6'
-                  : 'md:basis-1/3 lg:basis-1/5'
+                  ? 'sm:basis-1/3 md:basis-1/4 lg:basis-1/5 xl:basis-1/6'
+                  : 'sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5'
               }
             >
               <ProductCard
                 hideDetails={hideDetails}
-                hideAddToCart
+
                 hideBorder
                 product={product}
               />
